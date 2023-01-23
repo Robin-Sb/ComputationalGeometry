91226ca6ade4e8b4de780ecea8c2029e3fffe9ee
Robin-Sb <plixxeox@googlemail.com>
2023-01-23 22:04:57 +0100

    simplified 3 point formula

--- T4/miniball.py
+++ T4/miniball.py
@@ -57,24 +57,15 @@ class MSW:
         a3 = p1.x - p3.x
         b2 = p1.y - p2.y
         b3 = p1.y - p3.y
-        c2 = 0
-        c3 = 0
         d1 = p1.x ** 2 + p1.y ** 2
         d2 = d1 - p2.x ** 2 - p2.y ** 2
         d3 = d1 - p3.x ** 2 - p3.y ** 2
         ab = a3 * b2 - a2 * b3
         xa = (b2 * d3 - b3 * d2) / (ab * 2) - p1.x
-        xb = (b3 * c2 - b2 * c3) / ab
         ya = (a3 * d2 - a2 * d3) / (ab * 2) - p1.y
-        yb = (a2 * c3 - a3 * c2) / ab
-        A = xb ** 2 + yb ** 2 - 1
-        B = 2 * (xa * xb + ya * yb)
         C = xa ** 2 + ya ** 2
-        if A:
-            r = -((B + math.sqrt(B ** 2 - 4 * A * C)) / (2 * A))
-        else:
-            r = -(C / B)
-        return Circle(p1.x + xa + xb, p1.y + ya + yb, r)
+        r = -((math.sqrt(4 * C)) / -2)
+        return Circle(p1.x + xa, p1.y + ya, r)
 
     def enclosing_basis(self):
         b = self.basis
@@ -136,11 +127,8 @@ class DrawHandler:
         self.canvas_y = 600
         self.canvas = Canvas(self.window, width=self.canvas_x, height=self.canvas_y)
         self.canvas.pack()
-        # self.start_pos = None
-        # self.end_pos = None
         self.msw = MSW()
         self.canvas.bind("<Button-1>", self.handle_lclick)
-        #self.points = []
         self.circle_obj = None
         Button(self.window, text="Solve", command=self.solve).pack()
         self.window.mainloop()
@@ -159,9 +147,6 @@ class DrawHandler:
         point = Vec2(x, y)
         self.msw.points.append(point)
         self.draw_point(point)    
-        # circle = self.msw.test_circle()
-        # if circle:
-        #     self.draw_circle(circle)
 
     def solve(self):
         circle = self.msw.find_enclosing_circle()
