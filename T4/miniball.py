0491edfa0a55952e58b1090144d45d335b444f06
Robin-Sb <plixxeox@googlemail.com>
2023-01-23 21:58:23 +0100

    working minimum enclosing circle

--- T4/miniball.py
+++ T4/miniball.py
@@ -3,6 +3,8 @@ import math
 import random
 import time
 
+
+EPS = 0.0001
 class Vec2:
     def __init__(self, x, y) -> None:
         self.x = x
@@ -17,18 +19,25 @@ class Circle:
     def encloses(self, point):
         # maybe take care of floating point precision here
         dist = (point.x - self.x) ** 2 + (point.y - self.y) ** 2
-        if dist < self.r ** 2:
+        if dist < self.r ** 2 + EPS:
             return True
         return False
+
+    def encloses_all(self, points):
+        for point in points:
+            if not self.encloses(point):
+                return False
+        return True 
 
 class MSW:
     def __init__(self) -> None:
         self.points = []
         self.circle = None
+        self.basis = []
 
-    def encloses_all(self):
+    def encloses_all(self, circle):
         for point in self.points:
-            if not self.circle.encloses(point):
+            if not circle.encloses(point):
                 return False
         return True
     
@@ -40,7 +49,7 @@ class MSW:
     def construct_circle_2(self, p1, p2):
         x = (p1.x + p2.x) / 2
         y = (p1.y + p2.y) / 2
-        r = (p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2
+        r = math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) / 2
         return Circle(x, y, r)
 
     def construct_circle_3(self, p1, p2, p3):
@@ -62,16 +71,69 @@ class MSW:
         B = 2 * (xa * xb + ya * yb)
         C = xa ** 2 + ya ** 2
         if A:
-            r = (B + math.sqrt(B ** 2 - 4 * A * C)) / (2 * A)
+            r = -((B + math.sqrt(B ** 2 - 4 * A * C)) / (2 * A))
         else:
-            r = C / B
+            r = -(C / B)
         return Circle(p1.x + xa + xb, p1.y + ya + yb, r)
+
+    def enclosing_basis(self):
+        b = self.basis
+        if len(b) == 2:
+            return self.construct_circle_2(b[0], b[1])
+        else:
+            return self.construct_circle_3(b[0], b[1], b[2])
+
+    def extend_basis(self, p):
+        for point in self.basis:
+            circle = self.construct_circle_2(p, point)
+            if circle.encloses_all(self.basis):
+                self.basis = [point, p]
+                #self.circle = circle
+                return
+        for i in range(0, len(self.basis) - 1):
+            for j in  range(i + 1, len(self.basis)):
+                p1 = self.basis[i]
+                p2 = self.basis[j]
+                old_circle = self.construct_circle_2(p1, p2)
+                circle_opt_1 = self.construct_circle_2(p1, p)
+                circle_opt_2 = self.construct_circle_2(p2, p)
+                circle_opt_3 = self.construct_circle_3(p1, p2, p)
+                if (not old_circle.encloses(p) and 
+                    not circle_opt_1.encloses(p2) and 
+                    not circle_opt_2.encloses(p1) and 
+                    circle_opt_3.encloses_all([p, p1, p2])):
+                    self.basis = [p, p1, p2]
+                    return
+    
+    def check_if_in_basis(self,p):
+        for pb in self.basis:
+            if pb == p:
+                return True
+        return False
+
+    def find_enclosing_circle(self):
+        self.basis = [self.points[0], self.points[1]]
+        self.circle = self.construct_circle_2(self.basis[0], self.basis[1])
+        i = 0
+        n = len(self.points)
+        while i < n:
+            p = self.points[i]
+            if self.check_if_in_basis(p):
+                i += 1
+                continue
+            if self.circle and self.circle.encloses(p):
+                i += 1
+            else:
+                self.extend_basis(p)
+                self.circle = self.enclosing_basis()
+                i = 0
+        return self.circle
 
 class DrawHandler:
     def __init__(self) -> None:
         self.window = Tk()
-        self.canvas_x = 800
-        self.canvas_y = 800
+        self.canvas_x = 600
+        self.canvas_y = 600
         self.canvas = Canvas(self.window, width=self.canvas_x, height=self.canvas_y)
         self.canvas.pack()
         # self.start_pos = None
@@ -79,13 +141,17 @@ class DrawHandler:
         self.msw = MSW()
         self.canvas.bind("<Button-1>", self.handle_lclick)
         #self.points = []
+        self.circle_obj = None
+        Button(self.window, text="Solve", command=self.solve).pack()
         self.window.mainloop()
 
     def draw_point(self, point):
-        point_idx = self.canvas.create_oval(point.x -1, point.y - 1, point.x + 1, point.y + 1)
+        return self.canvas.create_oval(point.x -1, point.y - 1, point.x + 1, point.y + 1)
 
     def draw_circle(self, circle):
-        self.canvas.create_oval(circle.x + circle.r, circle.y + circle.r, circle.x - circle.r, circle.y - circle.r)
+        if self.circle_obj:
+            self.canvas.delete(self.circle_obj)
+        return self.canvas.create_oval(circle.x + circle.r, circle.y + circle.r, circle.x - circle.r, circle.y - circle.r)
 
     def handle_lclick(self, event):
         x = self.canvas.canvasx(event.x)
@@ -93,8 +159,12 @@ class DrawHandler:
         point = Vec2(x, y)
         self.msw.points.append(point)
         self.draw_point(point)    
-        circle = self.msw.test_circle()
-        if circle:
-            self.draw_circle(circle)
+        # circle = self.msw.test_circle()
+        # if circle:
+        #     self.draw_circle(circle)
+
+    def solve(self):
+        circle = self.msw.find_enclosing_circle()
+        self.circle_obj = self.draw_circle(circle)
 
 dh = DrawHandler()
